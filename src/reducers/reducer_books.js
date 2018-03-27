926f2a00f068b2704bc75c3e66f468d7f806be22
camimaya21 <camilamaya21@gmail.com>
2018-03-27 11:32:35 +0200

    BookDetail added

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,7 +1,7 @@
-export default function () {
+export default function() {
   return [
-    { title: 'The name of the wind' },
-    { title: 'The Fear of a Wise Man' },
-    { title: 'The Doors of Stone' }
+    { title: 'The name of the wind', pages: 1001 },
+    { title: 'The Fear of a Wise Man', pages: 1500 },
+    { title: 'The Doors of Stone', pages: 'unknow' }
   ];
 }
